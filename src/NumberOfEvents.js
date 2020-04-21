e03a7803cb30a6318d526c8eb1aeefd9089f376e
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2020-04-21 14:38:07 +0200

    Added ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
@@ -11,11 +11,23 @@ class NumberOfEvents extends Component {
         const value = event.target.value;
         this.setState({ numberOfEvents: value });
         this.props.updateEvents(null, null, value);
+
+        if (value  <= 0) {
+          this.setState({
+            infoText: 'The number needs to be at least 1',
+          });
+        } else {
+          this.setState({
+            infoText: '',
+          });
+        }
+
       }
 
   render() {
     return(
       <div className="number-of-events">
+        <ErrorAlert text={this.state.infoText} />
         <label>Number of events: </label>
         <input
           type="number"
