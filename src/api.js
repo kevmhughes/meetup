06f0ce75f90b87bf3e31a3903c36af282a00e6b4
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2020-04-21 18:49:24 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -64,6 +64,9 @@ async function getEvents(lat, lon, page) {
     }
     const result = await axios.get(url);
     const events = result.data.events;
+    if (events.length) { // Check if the events exist
+      localStorage.setItem('lastEvents', JSON.stringify(events));
+    }
     return events;
   }
 
